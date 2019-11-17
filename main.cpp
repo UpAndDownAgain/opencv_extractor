73b8ceafa407d623ad58559058b4b30d038b4151
UpAndDownAgain <musil.adam@gmail.com>
2019-11-17 16:28:00 +0100

    vytvoreni slozky pro output

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <opencv2/opencv.hpp>
+#include <sys/stat.h>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+int main(int argc, char **argv) {
+
+    if(argc < 2){
+        std::cout << "Zadejte cestu k video souboru jako argument pri spusteni" << std::endl;
+        return -1;
+    }
+
+    cv::VideoCapture cap(argv[1]);
+    const char *outputDirectory;
+
+    if(argc >= 3){
+        outputDirectory = argv[2];
+    }else{
+        outputDirectory = "output";
+    }
+
+    mkdir(outputDirectory, 0777);
+
+
+
     return 0;
 }
